40f9613447b83d330bd2ba9b17e6655c66e3ee3b
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-08 14:35:32 -0500

    Implement nameParse function to improve habitat display

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,22 @@
 function terrainParse(array) {
   const habitatOptions = [];
   array.forEach(location  => {
-    const name = location.name;
-    const urlSplit = location.url.split('/');
+    const name = nameParse(location.name),
+          urlSplit = location.url.split('/');
     habitatOptions.push({name,
                           id: urlSplit[urlSplit.length - 2]})
   })
   return habitatOptions;
 }
 
+function nameParse(string) {
+  const array = string.split('-'),
+        capitalized = array.map(word => word[0].toUpperCase() + word.slice(1));
+
+  if (capitalized.length > 1) return capitalized.join(' ');
+  return capitalized[0];
+}
+
 export {
-  terrainParse
+  terrainParse,
 }
