51e11af81e979309bdc39f255b6fd446ffd7fcd7
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-08 15:38:08 -0500

    Implement slight update to searchBox

--- src/components/List.js
+++ src/components/List.js
@@ -4,20 +4,21 @@ import { pokedexUrl } from '../helpers';
 export default function List({ pokemon, searchPending }) {
 
   if (searchPending) {
-    return (<div>Finding Pokemon...</div>)
+    return (<div>Finding Pokémon...</div>)
   }
 
   return (
     <div className="list">
       {pokemon.map(mon => {
         return (
-            <a
-              className="pokeCard"
-              href={pokedexUrl(mon.name, mon.id)}
-              rel="noreferrer"
-              target="_blank">
-                {mon.name}
-            </a>
+          <a
+            className="pokeCard"
+            href={pokedexUrl(mon.name, mon.id)}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {mon.name}
+          </a>
         )
       })}
     </div>
