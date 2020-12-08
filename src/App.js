0be3568b5a13d9cb02ca66717797fddad9aee9b4
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-08 14:09:56 -0500

    Install Axios and implement initial GET request for terrain options

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ habitats, setHabitats ] = useState('');
+
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon-habitat')
+        .then(data => {
+          const habitatOptions = [];
+          const terrain = data.data.results;
+          terrain.forEach(location  => {
+            const name = location.name;
+            const urlSplit = location.url.split('/');
+            habitatOptions.push({name,
+                                  id: urlSplit[urlSplit.length - 2]})
+          })
+          setHabitats(habitatOptions);
+        })
+  },[])
+
+  console.log(habitats);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
     </div>
   );
 }
